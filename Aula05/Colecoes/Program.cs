f699c2e8f092d59108a728e74bf85eff90fe39fa
camillofalcao <camillofalcao@gmail.com>
2021-01-08 19:36:04 -0300

    Adicionada uma List

--- Aula05/Colecoes/Program.cs
+++ Aula05/Colecoes/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Colecoes
 {
@@ -6,7 +7,34 @@ namespace Colecoes
     {
         static void Main(string[] args)
         {
+            //var idades = new int[5];
+            //idades[0] = 20;
+
+            //Lista
+            var idades = new List<int>();
+            idades.Add(20);
+            idades.Add(25);
+            idades.Add(50);
+            idades.Add(55);
+
+            Console.WriteLine($"Tamanho da lista: {idades.Count}");
+
+            Imprimir(idades);
+
+            if (idades.Contains(20))
+                Console.WriteLine("A idade 20 está presente.");
+
+            Console.ReadKey();
+        }
+
+        private static void Imprimir(List<int> idades)
+        {
+            Console.WriteLine("Idades informadas: ");
+
+            for (int i = 0; i < idades.Count; i++)
+                Console.Write($"{idades[i]}  ");
             
+            Console.WriteLine();
         }
     }
 }
