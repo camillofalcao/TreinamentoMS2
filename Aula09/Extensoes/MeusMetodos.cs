9e6ad6c56a3e242145b8b819e46e1b7bc45eaf4f
camillofalcao <camillofalcao@gmail.com>
2021-01-14 20:06:19 -0300

    Extension Methods - exemplo

---
+++ Aula09/Extensoes/MeusMetodos.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Extensoes
+{
+    static class MeusMetodos
+    {
+        public static IEnumerable<T> Selecionar<T>(this IEnumerable<T> objetos, Func<T, bool> selecionar)
+        {
+            var resultados = new List<T>();
+
+            foreach (var obj in objetos)
+                if (selecionar(obj))
+                    resultados.Add(obj);
+
+            return resultados;
+        }
+
+        public static string AdicionarAspas(this string texto)
+        {
+            return "\"" + texto + "\"";
+        }
+
+        public static string RemoverTexto(this string texto, string textoASerRemovido)
+        {
+            return texto.Replace(textoASerRemovido, "");
+        }
+
+        public static int ContaVogais(this string texto)
+        {
+            var qtdeVogais = 0;
+
+            foreach (var caractere in texto)
+                if (EhVogal(caractere))
+                    qtdeVogais++;
+
+            return qtdeVogais;
+        }
+
+        public static bool EhVogal(this char caractere)
+        {
+            return "aeiouAEIOU".Contains(caractere);
+        }
+    }
+}
---
+++ Aula09/Extensoes/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Extensoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string texto = "Juiz de Fora";
+
+            //Console.WriteLine(MeusMetodos.AdicionarAspas(texto));
+            Console.WriteLine(texto.AdicionarAspas());
+
+            //Console.WriteLine($"O caractere {texto[0]} {(MeusMetodos.EhVogal(texto[0]) ? "é uma vogal" : "não é uma vogal")}.");
+            Console.WriteLine($"O caractere {texto[0]} {(texto[0].EhVogal() ? "é uma vogal" : "não é uma vogal")}.");
+
+            //Console.WriteLine($"Existem {MeusMetodos.ContaVogais(texto)} vogais em '{texto}'.");
+            Console.WriteLine($"Existem {texto.ContaVogais()} vogais em '{texto}'.");
+
+            Console.WriteLine("Removendo a string 'Fora': " + texto.RemoverTexto("Fora"));
+
+            var vet = new int[] { 1, 10, 5, 15, 12, 29, 30 };
+
+            var numerosDivisiveisPor5 = vet.Selecionar(x => x % 5 == 0);
+
+            Console.WriteLine("Números do vetor divisíveis por 5: ");
+
+            foreach (var item in numerosDivisiveisPor5)
+                Console.Write($"{item}  ");
+            
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+    }
+}
