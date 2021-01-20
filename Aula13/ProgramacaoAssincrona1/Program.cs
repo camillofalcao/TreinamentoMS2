94be0b5b384d1b5a774eda403be252d67307241a
camillofalcao <camillofalcao@gmail.com>
2021-01-20 20:52:21 -0300

    Testes usando programação assíncrona

---
+++ Aula13/ProgramacaoAssincrona1/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProgramacaoAssincrona1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"{ProcessamentoSincrono()} raízes inteiras (Síncrono)\n");
+
+            Console.WriteLine($"{ProcessamentoAssincrono()} raízes inteiras (Parallel.For)\n");
+
+            Console.WriteLine($"{ProcessamentoAssincronoUsandoTask()} raízes inteiras (Task)\n");
+
+            Console.WriteLine($"{ProcessamentoAssincronoUsandoPLinq()} raízes inteiras (PLINQ)\n");
+
+            Console.ReadKey();
+        }
+
+        private static int ProcessamentoSincrono()
+        {
+            var inicio = DateTime.Now;
+
+            var cont = Processamento(0, MAX);
+
+            Console.WriteLine($"Síncrono: {(DateTime.Now - inicio).TotalSeconds} segundos.");
+
+            return cont;
+        }
+
+        private static int ProcessamentoAssincrono()
+        {
+            int div = MAX / NUMERO_THREADS;
+            DateTime inicio = DateTime.Now;
+
+            int[] resultados = new int[NUMERO_THREADS];
+            int tot;
+
+            Parallel.For(0, NUMERO_THREADS, i => resultados[i] = Processamento(i * div, (i + 1) * div));
+
+            tot = resultados.Sum();
+
+            Console.WriteLine($"Assíncrono: {(DateTime.Now - inicio).TotalSeconds} segundos.");
+
+            return tot;
+        }
+
+        private static int ProcessamentoAssincronoUsandoTask()
+        {
+            var div = MAX / NUMERO_THREADS;
+            var inicio = DateTime.Now;
+
+            var resultados = new Task<int>[NUMERO_THREADS];
+
+            int tot;
+
+            for (int i = 0; i < NUMERO_THREADS; i++)
+            {
+                resultados[i] = ProcessamentoAsync(i * div, (i + 1) * div);
+            }
+
+            Task.WaitAll(resultados);
+
+            tot = resultados.Sum(x => x.Result);
+
+            Console.WriteLine($"Assíncrono com Task: {(DateTime.Now - inicio).TotalSeconds} segundos.");
+
+            return tot;
+        }
+
+        private async static Task<int> ProcessamentoAsync(int inicio, int fim)
+        {
+            int cont = 0;
+            double raiz;
+
+            await Task.Run(() =>
+            {
+                for (int i = inicio; i < fim; i++)
+                {
+                    raiz = Math.Sqrt(i);
+                    if (raiz == Math.Floor(raiz))
+                        cont++;
+                }
+            });
+
+            return cont;
+        }
+
+        private static int ProcessamentoAssincronoUsandoPLinq()
+        {
+            var inicio = DateTime.Now;
+            var div = MAX / NUMERO_THREADS;
+
+            int tot = Enumerable.Range(0, NUMERO_THREADS).AsParallel().Sum((index) => Processamento(index * div, (index + 1) * div));
+            
+            Console.WriteLine($"Assíncrono com PLinq: {(DateTime.Now - inicio).TotalSeconds} segundos.");
+
+            return tot;
+        }
+
+        private static int Processamento(int inicio, int fim)
+        {
+            int cont = 0;
+            double raiz;
+
+            for (int i = inicio; i < fim; i++)
+            {
+                raiz = Math.Sqrt(i);
+                if (raiz == Math.Floor(raiz))
+                    cont++;
+            }
+
+            return cont;
+        }
+
+        private const int MAX = 1000000000;
+        private const int NUMERO_THREADS = 10;
+    }
+}
---
+++ Aula13/TestesDiversos/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestesDiversos
+{
+    class Program
+    {
+        //async static Task<int> Processar()
+        //{
+        //    int soma = 0;
+
+        //    Console.WriteLine("Log 1.1");
+
+        //    await Task.Run(() =>
+        //    {
+        //        for (int i = 0; i < 10000000; i++)
+        //        {
+        //            if (i % 7 == 0)
+        //                soma += i;
+        //        }
+        //    });
+
+        //    Console.WriteLine("Log 1.2");
+
+        //    return soma;
+        //}
+
+        //async static Task BaixarArquivo(string url, string caminho)
+        //{
+        //    Console.WriteLine($"Baixando arquivo '{url}'...");
+
+        //    await Task.Run(() =>
+        //    {
+        //        System.Threading.Thread.Sleep(2000);
+        //        Console.WriteLine($"Arquivo '{url} baixado em '{caminho}'");
+        //    });
+        //}
+
+        static void Main(string[] args)
+        {
+            var lista = Enumerable.Range(100, 10);
+
+            Console.ReadKey();
+
+
+            //var url = "http://exemplo.com/arquivo.zip";
+            //var caminho = @"C:\Temp\caminho.zip";
+
+            //Console.WriteLine("Programa iniciou o download...");
+
+            //var tarefaBaixa = BaixarArquivo(url, caminho);
+
+            //Console.WriteLine($"Baixando arquivo '{url}'...");
+
+            //var tarefaBaixa = Task.Run(() =>
+            //{
+            //    System.Threading.Thread.Sleep(2000);
+            //    Console.WriteLine($"Arquivo '{url} baixado em '{caminho}'");
+            //});
+
+            //Console.WriteLine("Programa fez algo.");
+
+            //Console.WriteLine("Programa fez outra coisa.");
+
+            //Console.WriteLine("Agora o programa aguardará o término do download");
+
+            //tarefaBaixa.Wait(5000);
+
+            //Console.WriteLine("Fim do programa");
+
+            //Console.ReadKey();
+
+            //int soma;
+
+            //Console.WriteLine("Log: 1");
+
+            //var resultadoProcessamento = Processar();
+
+            //Console.WriteLine("Log: 2");
+
+            //resultadoProcessamento.Wait();
+
+            //soma = resultadoProcessamento.Result;
+
+            //Console.WriteLine($"Soma: {soma}");
+
+            //Console.ReadKey();
+
+
+
+
+            //var dataInicial = DateTime.Now;
+
+            //Console.WriteLine("Tecle <Enter> o mais rápido possível");
+
+            //Console.ReadKey();
+
+            //var dataFinal = DateTime.Now;
+
+            //TimeSpan intervaloDeTempo = (dataFinal - dataInicial);
+
+            //Console.WriteLine($"Data inicial: {dataInicial.ToShortDateString()} {dataInicial.ToLongTimeString()}");
+            //Console.WriteLine($"Data final: {dataFinal.ToShortDateString()} {dataFinal.ToLongTimeString()}");
+
+            //Console.WriteLine($"Diferença em segundos: {intervaloDeTempo.TotalSeconds}");
+
+            Console.ReadKey();
+        }
+    }
+}
