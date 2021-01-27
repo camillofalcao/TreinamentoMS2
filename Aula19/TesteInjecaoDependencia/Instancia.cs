e242ecaba5740fba1385260e12c71cf29416dd09
camillofalcao <camillofalcao@gmail.com>
2021-01-27 20:34:54 -0300

    Exemplo de injeção de dependência

---
+++ Aula19/TesteInjecaoDependencia/Instancia.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TesteInjecaoDependencia
+{
+    static class Instancia
+    {
+        static Instancia()
+        {
+            IServiceCollection services = new ServiceCollection()
+                .AddSingleton<ISaudacao, SaudacaoAmigavel>()
+                .AddTransient<IRecepcao, RecepcaoDireta>();
+
+            provedor = services.BuildServiceProvider();
+        }
+
+        public static T Get<T>() => provedor.GetService<T>();
+
+        private static ServiceProvider provedor;
+    }
+}
---
+++ Aula19/TesteInjecaoDependencia/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TesteInjecaoDependencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+
+            Console.Write("Informe o seu nome: ");
+            nome = Console.ReadLine();
+
+            var recepcao = Instancia.Get<IRecepcao>();
+            recepcao.Recepcionar(nome);
+
+            Console.ReadKey();
+        }
+    }
+}
